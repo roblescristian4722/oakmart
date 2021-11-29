4290aea2afc9f091bebff633f298016453660bf1
roblescristian4722 <roblescristian4722@gmail.com>
2021-11-28 18:13:55 -0600

    basic style added to Purchases

--- src/Views/MenuViews/Purchases.js
+++ src/Views/MenuViews/Purchases.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 import { View, Text, FlatList, StyleSheet } from 'react-native'
 
 import colors from '../../Components/colors'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import AsyncStorage from '@react-native-community/async-storage';
+
+import { Dimensions } from 'react-native'
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
 import Storage from 'react-native-storage';
 const storage = new Storage({
   storageBackend: AsyncStorage,
@@ -62,14 +67,23 @@ export default class Purchases extends Component {
 
   renderItem = ({item}) => {
     return (
-      <View>
+      <View style={style.item_container}>
+        {item.images
+        ? <Image source={{uri: item.images}} style={style.item_img}/>
+        :<Ionicons name='eye-off' style={style.item_img_mis}/>}
+        <View style={style.item_info_container}>
+          <Text style={style.item_text}>{item.product_info.name}</Text>
+          <Text style={style.item_text}>${item.price}</Text>
+          <Text style={style.item_text}>Cantidad: {item.pieces}</Text>
+          <Text style={style.item_text}>Total: ${item.price_total}</Text>
+        </View>
       </View>
   )}
 
   render() {
     if (this.state.data !== null)
       return (
-        <View>
+        <View style={style.container}>
           <FlatList
             style={style.list}
             data={this.state.data}
@@ -88,5 +102,39 @@ export default class Purchases extends Component {
 }
 
 const style = StyleSheet.create({
-
+  container: {
+    height: '100%',
+    backgroundColor: colors.secondaryBg,
+  },
+  list: {
+    alignSelf: 'center',
+    backgroundColor: colors.text,
+    borderWidth: 1,
+    width: '90%',
+    height: '90%',
+    marginBottom: '5%',
+    marginTop: '5%',
+  },
+  item_container: {
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  item_img: {
+    height: 100,
+    width: 100,
+    alignSelf: 'center',
+  },
+  item_img_mis: {
+    backgroundColor: colors.placeholder,
+    padding: '8%',
+    fontSize: 0.10 * SCREEN_WIDTH,
+    alignSelf: 'center',
+  },
+  item_info_container: {
+    padding: '3%',
+  },
+  item_text: {
+    fontSize: 16,
+    color: 'black',
+  }
 })
